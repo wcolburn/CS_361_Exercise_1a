b5b7284039c01c81c0d9ca930ac9887b42b936c8
Liam Colburn <colburnw@oregonstate.edu>
2023-10-03 16:28:47 -0700

    Add loop to print each Player object

--- main.rb
+++ main.rb
@@ -7,12 +7,17 @@ class Player
   def print_self
     puts "Ready #{@name}! Score: #{@score}"
   end
-
 end
 
 players = Array.new
 
+# Instantiates Player objects
 (1..50).each { |i|
-  player = Player.new("Player #{i}", 10)
+  player = Player.new("Player #{i}", rand(10..300))
   players.push(player)
+}
+
+# Loops over each Player object and displays info
+players.each { |player|
+  player.print_self
 }
